3d989b355f0402c3e72820206702ef73ac2b36b7
FoxFox123 <khangba153@gmail.com>
2026-02-28 20:21:40 +0700

    Add solution file and Database folder

---
+++ Database/Database.sql
@@ -0,0 +1,48 @@
+﻿USE MovieBookingDB;
+GO
+
+-- =============================================
+-- XÓA BẢNG MOVIES CŨ
+-- =============================================
+DROP TABLE IF EXISTS Movies;
+GO
+
+
+
+-- =============================================
+-- KIỂM TRA BẢNG ĐÃ TẠO THÀNH CÔNG
+-- =============================================
+SELECT 
+    COLUMN_NAME,
+    DATA_TYPE,
+    CHARACTER_MAXIMUM_LENGTH,
+    IS_NULLABLE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'Movies'
+ORDER BY ORDINAL_POSITION;
+GO
+
+
+-- Kiểm tra dữ liệu đã thêm
+--SELECT 
+   -- Chọn dữ liệu cần xemm
+--FROM --Tên bảng;
+--GO
+
+
+
+-- Xóa tất cả dữ liệu (vẫn giữ cấu trúc bảng)
+DELETE FROM Bookings;
+GO
+
+-- Reset IDENTITY về 1
+DBCC CHECKIDENT ('Movies', RESEED, 0);
+GO
+
+-- Kiểm tra đã xóa hết chưa
+SELECT COUNT(*) AS TotalRecords FROM Movies;
+GO
+
+
+
+
